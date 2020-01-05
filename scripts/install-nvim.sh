71461d014c20ef3806195aa9d8195ef5ba613456
siteyo <phicyanoc@gmail.com>
2020-01-05 18:02:39 +0900

    Update

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Create directories
 mkdir -p ~/.config/nvim
 mkdir -p ~/.nvim
 mkdir -p ~/.nvim/backup
 mkdir -p ~/.nvim/swap
 mkdir -p ~/.nvim/undo
 
+# Create a local setting file
 touch ~/.nvim/local_vimrc.vim
 
+# Create symbolic links
 ln -sf ~/dotfiles/vim/vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/vim/gvimrc ~/.config/nvim/ginit.vim
 ln -sf ~/dotfiles/vim/coc-settings.json ~/.config/nvim/coc-settings.json
+
+# Install plugins
+nvim +PlugInstall +qall
--- scripts/install-vim.sh
+++ scripts/install-vim.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Create directories
 mkdir -p ~/.config/vim
 mkdir -p ~/.vim
 mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/swap
 mkdir -p ~/.vim/undo
 
+# Create a local setting file
 touch ~/.vim/local_vimrc.vim
 
+# Create symbolic links
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/vim/coc-settings.json ~/.config/vim/coc-settings.json
+
+# Install plugins
+vim +PlugInstall +qall
