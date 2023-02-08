4bd0728d2d51efec8c2980b25c3dd8b031dd01e9
Saito Yuki <phicyanoc@gmail.com>
2023-02-08 23:33:33 +0900

    Update

--- config/nvim/lua/config/plugins/neogit.lua
+++ config/nvim/lua/config/plugins/neogit.lua
@@ -22,6 +22,12 @@ M.config = function()
   vim.keymap.set("n", "[Git]c", function()
     neogit.open({ "commit" })
   end)
+  vim.keymap.set("n", "[Git]l", function()
+    neogit.open({ "log" })
+  end)
+  vim.keymap.set("n", "[Git]h", function()
+    neogit.open({ "branch" })
+  end)
 end
 
 return M
