a1aa7a048a711f92063a013abc12d8eb03d26c77
Saito Yuki <phicyanoc@gmail.com>
2023-02-08 11:51:20 +0900

    Update

--- config/nvim/lua/config/plugins/indent-blankline.lua
+++ config/nvim/lua/config/plugins/indent-blankline.lua
@@ -2,8 +2,9 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   config = function()
     require("indent_blankline").setup({
-      show_current_context = true,
+      show_current_context = false,
+      show_current_context_start = false,
     })
   end,
-  event = "BufReadPost",
+  event = "BufReadPre",
 }
--- config/nvim/lua/config/plugins/mini.lua
+++ config/nvim/lua/config/plugins/mini.lua
@@ -1,37 +1,57 @@
-local M = {
-  "echasnovski/mini.animate",
-  event = "VeryLazy",
+return {
+  -- common animation
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    config = function()
+      local mouse_scrolled = false
+      for _, scroll in ipairs({ "Up", "Down" }) do
+        local key = "<ScrollWheel" .. scroll .. ">"
+        vim.keymap.set("", key, function()
+          mouse_scrolled = true
+          return key
+        end, { expr = true })
+      end
+
+      local animate = require("mini.animate")
+
+      animate.setup({
+        resize = {
+          timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+        },
+        scroll = {
+          timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+          subscroll = animate.gen_subscroll.equal({
+            predicate = function(total_scroll)
+              if mouse_scrolled then
+                mouse_scrolled = false
+                return false
+              end
+              return total_scroll > 1
+            end,
+          }),
+        },
+      })
+    end,
+  },
+
+  -- visualize indent scope
+  {
+    "echasnovski/mini.indentscope",
+    event = "BufReadPre",
+    config = function()
+      require("mini.indentscope").setup({
+        symbol = "│",
+      })
+    end,
+  },
+
+  -- buffer Removing
+  {
+    "echasnovski/mini.bufremove",
+    event = "BufReadPre",
+    config = function()
+      require("mini.bufremove").setup()
+    end,
+  },
 }
-
-M.config = function()
-  local mouse_scrolled = false
-  for _, scroll in ipairs({ "Up", "Down" }) do
-    local key = "<ScrollWheel" .. scroll .. ">"
-    vim.keymap.set("", key, function()
-      mouse_scrolled = true
-      return key
-    end, { expr = true })
-  end
-
-  local animate = require("mini.animate")
-
-  animate.setup({
-    resize = {
-      timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
-    },
-    scroll = {
-      timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
-      subscroll = animate.gen_subscroll.equal({
-        predicate = function(total_scroll)
-          if mouse_scrolled then
-            mouse_scrolled = false
-            return false
-          end
-          return total_scroll > 1
-        end,
-      }),
-    },
-  })
-end
-
-return M
