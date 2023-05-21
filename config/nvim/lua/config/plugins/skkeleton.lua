c4bf32bbb94bc12d17c8c157cc095ab87986cca0
Saito Yuki <phicyanoc@gmail.com>
2023-05-21 22:53:45 +0900

    Update

--- config/nvim/lua/config/plugins/skkeleton.lua
+++ config/nvim/lua/config/plugins/skkeleton.lua
@@ -1,25 +1,41 @@
-local M = {
-  "vim-skk/skkeleton",
-  event = "InsertEnter",
-  dependencies = {
-    "vim-denops/denops.vim",
-  },
-  config = function()
-    vim.cmd([[
-      imap <C-j> <Plug>(skkeleton-enable)
-      cmap <C-j> <Plug>(skkeleton-enable)
-      call skkeleton#config({
-        \ 'globalJisyo': '~/.local/share/skk/SKK-JISYO.L'
-        \ })
+return {
+  {
+    "vim-skk/skkeleton",
+    event = "InsertEnter",
+    dependencies = {
+      "vim-denops/denops.vim",
+    },
+    config = function()
+      vim.cmd([[
+    imap <C-j> <Plug>(skkeleton-enable)
+    cmap <C-j> <Plug>(skkeleton-enable)
+    call skkeleton#config({
+      \ 'globalJisyo': '~/.local/share/skk/SKK-JISYO.L'
+      \ })
       call skkeleton#config({
         \ 'globalDictionaries': [
         \ '~/.local/share/skk/SKK-JISYO.L',
         \ '~/.local/share/skk/SKK-JISYO.jinmei',
         \ ]
         \ })
-      call skkeleton#register_keymap('henkan', '<CR>', 'kakutei')
-    ]])
-  end,
+        call skkeleton#register_keymap('henkan', '<CR>', 'kakutei')
+        ]])
+    end,
+  },
+  {
+    "delphinus/skkeleton_indicator.nvim",
+    dependencies = {
+      "vim-skk/skkeleton",
+    },
+    config = function()
+      require("skkeleton_indicator").setup({
+        eijiHlName = "Function",
+        hiraHlName = "String",
+        kataHlName = "Todo",
+        hankataHlName = "Special",
+        zenkakuHlName = "Search",
+      })
+    end,
+    event = "InsertEnter",
+  },
 }
-
-return M
