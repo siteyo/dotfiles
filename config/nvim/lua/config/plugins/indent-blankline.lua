71998a362ba0bc94fa57fe47cbbdc02ef474d0d1
Saito Yuki <phicyanoc@gmail.com>
2023-09-28 22:00:20 +0900

    Update

--- config/nvim/lua/config/plugins/indent-blankline.lua
+++ config/nvim/lua/config/plugins/indent-blankline.lua
@@ -1,6 +1,8 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   opts = {
+    indent = { char = "│" },
     show_current_context = false,
     show_current_context_start = false,
     filetype_exclude = { "help", "alpha", "neo-tree", "Trouble", "lazy", "mind" },
