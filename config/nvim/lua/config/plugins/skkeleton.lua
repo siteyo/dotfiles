5d85d56836f13e8688aabb9248d340a8bd8e92c2
Saito Yuki <phicyanoc@gmail.com>
2023-09-29 22:26:27 +0900

    Update

--- config/nvim/lua/config/plugins/skkeleton.lua
+++ config/nvim/lua/config/plugins/skkeleton.lua
@@ -9,18 +9,26 @@ return {
       vim.cmd([[
         imap <C-j> <Plug>(skkeleton-enable)
         cmap <C-j> <Plug>(skkeleton-enable)
+        call skkeleton#register_kanatable('azik', {}, v:true)
         call skkeleton#config({
-          \ 'globalJisyo': '~/.local/share/skk/SKK-JISYO.L'
-          \ })
-        call skkeleton#config({
+          \ 'kanaTable': 'azik',
+          \ 'globalJisyo': '~/.local/share/skk/SKK-JISYO.L',
           \ 'globalDictionaries': [
-          \ '~/.local/share/skk/SKK-JISYO.L',
-          \ '~/.local/share/skk/SKK-JISYO.jinmei',
-          \ ]
+          \   '~/.local/share/skk/SKK-JISYO.L',
+          \   '~/.local/share/skk/SKK-JISYO.jinmei',
+          \ ],
+          \ 'globalKanaTableFiles': ['~/.local/share/skk/azik_us.rule'],
           \ })
         call skkeleton#register_keymap('henkan', '<CR>', 'kakutei')
-        call skkeleton#register_kanatable('rom', {
-          \ 'zx': ['ー'],
+        call skkeleton#register_keymap('input', '/', 'abbrev')
+        call skkeleton#register_keymap('input', '<S-l>', 'zenkaku')
+        call skkeleton#register_kanatable('azik', {
+          \   ',': ['、', ''],
+          \   '.': ['。', ''],
+          \   'l': 'disable',
+          \   'q': 'katakana',
+          \   ';': 'henkanPoint',
+          \   ' ': 'henkanFirst',
           \ })
       ]])
     end,
--- scripts/install-skk-dict.sh
+++ scripts/install-skk-dict.sh
@@ -3,7 +3,11 @@
 main() {
   set -euo pipefail
   source "$(git rev-parse --show-toplevel)/scripts/util.sh"
-  print_info '==> Install skk dict ...'
+  print_info '==> Install skk dict and setup azik ...'
+
+  # Change current directory
+  current_dir=$(dirname "${BASH_SOURCE[0]}")
+  dotfiles_dir=$(builtin cd "${current_dir}" && git rev-parse --show-toplevel)
 
   # Create directory
   mkdir -pv ~/.local/share/skk
@@ -13,6 +17,17 @@ main() {
     gunzip -c >"${HOME}/.local/share/skk/SKK-JISYO.L"
   curl https://skk-dev.github.io/dict/SKK-JISYO.jinmei.gz |
     gunzip -c >"${HOME}/.local/share/skk/SKK-JISYO.jinmei"
+
+  # Setup azik
+  ln -sfv "${dotfiles_dir}/etc/azik_us.rule" "${HOME}/.local/share/skk/azik_us.rule"
+
+  # Setup corvusskk
+  if uname -a | grep -q microsoft; then
+    winhome="$(wslpath "$(wslvar USERPROFILE)")"
+    mkdir -pv "${winhome}/AppData/Roaming/CorvusSKK"
+    mv -v "${winhome}/AppData/Roaming/CorvusSKK/config.xml" "${winhome}/AppData/Roaming/CorvusSKK/config-bk.xml"
+    cp -fv "${dotfiles_dir}/etc/corvusskk-config.xml" "${winhome}/AppData/Roaming/CorvusSKK/config.xml"
+  fi
 
   print_done
 }
