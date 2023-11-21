8767f7b446896d0fc6e1317bb3e02cb857630770
Saito Yuki <phicyanoc@gmail.com>
2023-11-21 22:33:09 +0900

    Update

--- config/nvim/lua/config/cli/node_servers/init.lua
+++ config/nvim/lua/config/cli/node_servers/init.lua
@@ -2,22 +2,38 @@ local M = {
   name = "node_servers",
   dir = vim.fn.fnamemodify(debug.getinfo(1).source:sub(2), ":h"),
   dependencies = { "nvim-lua/plenary.nvim" },
-  event = "BufReadPost",
-  build = "npm i",
+  event = "VeryLazy",
+  build = "npm install"
 }
 
 M.config = function(spec)
   local Path = require("plenary.path")
+  local Job = require("plenary.job")
+
   local dir = spec.dir
+  local npm_install_job = Job:new({
+    command = "npm",
+    args = { "install" },
+    cwd = dir,
+    on_start = function()
+      vim.notify("[Node Servers] Installing...")
+    end,
+    on_exit = function()
+      vim.notify("[Node Servers] Node Servers have been installed.")
+    end,
+  })
+
   local node_modules_dir = Path:new(dir, "node_modules")
   if not node_modules_dir:exists() then
-    vim.fn.system({ "npm", "i", "--prefix", dir })
+    npm_install_job:start()
   end
+
   local bin_dir = Path:new(dir, "node_modules", ".bin")
-  if not bin_dir:exists() then
-    bin_dir:mkdir({ parents = true })
-  end
   vim.env.PATH = bin_dir:absolute() .. ":" .. vim.env.PATH
+
+  vim.api.nvim_create_user_command("NSInstall", function()
+    npm_install_job:start()
+  end, {})
 end
 
 return M
---
+++ config/nvim/lua/config/cli/python_tools/init.lua
@@ -0,0 +1,39 @@
+local M = {
+  name = "python_tools",
+  dir = vim.fn.fnamemodify(debug.getinfo(1).source:sub(2), ":h"),
+  dependencies = { "nvim-lua/plenary.nvim" },
+  event = "VeryLazy",
+  build = "rye sync",
+}
+
+M.config = function(spec)
+  local Path = require("plenary.path")
+  local Job = require("plenary.job")
+
+  local dir = spec.dir
+  local rye_sync_job = Job:new({
+    command = "rye",
+    args = { "sync" },
+    cwd = dir,
+    on_start = function()
+      vim.notify("[Python Tools] Installing...")
+    end,
+    on_exit = function()
+      vim.notify("[Python Tools] Tools have been installed.")
+    end,
+  })
+
+  local venv_dir = Path:new(dir, ".venv")
+  if not venv_dir:exists() then
+    rye_sync_job:start()
+  end
+
+  local bin_dir = Path:new(dir, ".venv", "bin")
+  vim.env.PATH = bin_dir:absolute() .. ":" .. vim.env.PATH
+
+  vim.api.nvim_create_user_command("PTInstall", function()
+    rye_sync_job:start()
+  end, {})
+end
+
+return M
