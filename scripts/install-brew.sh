6463d6b4baad4771f253d29ad99d8e07ae214b7d
Saito Yuki <phicyanoc@gmail.com>
2024-01-06 14:58:36 +0900

    zsh: update homebrew installation.

--- scripts/install-brew.sh
+++ scripts/install-brew.sh
@@ -23,6 +23,12 @@ main() {
 
   if [ "$(uname)" == 'Linux' ]; then
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  elif [ "$(uname)" == 'Darwin' ]; then
+    if [ -f '/opt/homebrew/bin/brew' ]; then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    elif [ -f '/usr/local/bin/brew' ]; then
+      eval "$(/usr/local/bin/brew shellenv)"
+    fi
   fi
 
   [ -f "${HOME}/.Brewfile" ] && brew bundle --global
