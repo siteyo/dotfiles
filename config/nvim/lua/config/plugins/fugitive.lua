193fe8ea9442997ddab37061680f83a617223da6
Saito Yuki <phicyanoc@gmail.com>
2024-01-20 21:08:21 +0900

    neovim: update fugitive mappings

--- config/nvim/lua/config/plugins/fugitive.lua
+++ config/nvim/lua/config/plugins/fugitive.lua
@@ -3,13 +3,13 @@ local M = {
   enabled = true,
   cmd = { "Git", "G" },
   keys = {
-    { "<Leader>gs", "<Cmd>Git<CR>" },
-    { "<Leader>gc", "<Cmd>Git commit<CR>" },
-    { "<Leader>gb", "<Cmd>Git blame<CR>" },
-    -- { "<Leader>gm", "<Cmd>Git merge<CR>" },
-    { "<Leader>gh", "<Cmd>Git branch<CR>" },
-    { "<Leader>gll", "<Cmd>Git log --graph<CR>" },
-    { "<Leader>gla", "<Cmd>Git log --graph --all --decorate=full<CR>" },
+    { "<Leader>gs", "<Cmd>Git<CR>", mode = { "n" } },
+    { "<Leader>gc", "<Cmd>Git commit<CR>", mode = { "n" } },
+    { "<Leader>gb", "<Cmd>Git blame<CR>", mode = { "n" } },
+    -- { "<Leader>gm", "<Cmd>Git merge<CR>", mode = { "n"} },
+    { "<Leader>gh", "<Cmd>Git branch<CR>", mode = { "n" } },
+    { "<Leader>gll", "<Cmd>Git log --graph<CR>", mode = { "n" } },
+    { "<Leader>gla", "<Cmd>Git log --graph --all --decorate=full<CR>", mode = { "n" } },
   },
 }
 
