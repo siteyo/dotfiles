f919f1e806c48a6a2658586fddb39952a48f534d
Saito Yuki <phicyanoc@gmail.com>
2024-02-08 21:10:16 +0900

    neovim: add command

---
+++ config/nvim/lua/config/commands.lua
@@ -0,0 +1,15 @@
+-- for macOS
+if vim.fn.has("mac") then
+  vim.api.nvim_create_user_command("OpenFinder", function()
+    vim.fn.jobstart({ "open", vim.fn.expand("%:h") }, {
+      on_exit = function()
+        vim.notify("Open Finder")
+      end,
+    })
+  end, {})
+end
+
+-- for win
+-- if vim.fn.has("wsl") then
+--   -- code
+-- end
