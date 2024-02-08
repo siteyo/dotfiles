9d7f5ed66ae0dbaaf21aaf7c529ac08eaa49e03a
Saito Yuki <phicyanoc@gmail.com>
2024-02-08 22:15:51 +0900

    neovim: add autocmd for neorg

--- config/nvim/lua/config/autocmd.lua
+++ config/nvim/lua/config/autocmd.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank({ higroup = "InSearch", timeout = 500 })
-  end,
+local neorg = vim.api.nvim_create_augroup("neorg", { clear = true })
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "norg",
+  group = neorg,
+  command = "setlocal conceallevel=1",
 })
