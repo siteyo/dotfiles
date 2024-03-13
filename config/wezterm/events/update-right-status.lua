d5f1769b3e1b5fbfff911241922bcbfa24ddfb53
Saito Yuki <phicyanoc@gmail.com>
2024-03-13 09:03:53 +0900

    wezterm: add battery status

--- config/wezterm/events/update-right-status.lua
+++ config/wezterm/events/update-right-status.lua
@@ -2,10 +2,17 @@ local wez = require("wezterm")
 local color = require("color")
 
 wez.on("update-status", function(window)
-  local date = wez.strftime("%Y/%m/%d (%a) %H:%M:%S")
+  local date = " " .. wez.strftime("%Y/%m/%d (%a) %H:%M:%S") .. " "
+  local bat = ""
+  for _, b in ipairs(wez.battery_info()) do
+    bat = " " .. string.format("%.0f%%", b.state_of_charge * 100) .. " "
+  end
   window:set_right_status(wez.format({
-    { Background = { Color = color.background:lighten(0.3) } },
+    { Background = { Color = color.background:lighten(0.2) } },
     { Foreground = { Color = color.foreground } },
     { Text = date },
+    { Background = { Color = color.background:lighten(0.4) } },
+    { Foreground = { Color = color.foreground } },
+    { Text = bat },
   }))
 end)
