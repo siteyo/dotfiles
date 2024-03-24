75e5ba678911656d0fdc4e8896f757264256de14
Saito Yuki <phicyanoc@gmail.com>
2024-03-24 21:58:56 +0900

    neovim: add flash.nvim mapping

--- config/nvim/lua/config/plugins/flash.lua
+++ config/nvim/lua/config/plugins/flash.lua
@@ -95,6 +95,31 @@ local M = {
       end,
     },
     {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Treesitter Search",
+    },
+    {
+      "<c-s>",
+      mode = { "c" },
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search",
+    },
+
+    {
       "fw",
       mode = { "n", "x", "o" },
       function()
