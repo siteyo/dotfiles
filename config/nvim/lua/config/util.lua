fc682328e63a30d931c680829a79aac750b76fba
Saito Yuki <phicyanoc@gmail.com>
2024-06-04 23:52:48 +0900

    neovim: modified general settings

--- config/nvim/lua/config/util.lua
+++ config/nvim/lua/config/util.lua
@@ -1,5 +1,9 @@
+------------------------------------------------------------
+--- util.lua
+------------------------------------------------------------
 local M = {}
 
+-- Get appearance mode
 M.appearance = function()
   if vim.fn.has("mac") then
     local color = vim.fn.system("defaults read -g AppleInterfaceStyle")
@@ -10,6 +14,48 @@ M.appearance = function()
     end
   else
     return "dark"
+  end
+end
+
+-- Set submode mapping
+---@param mode string | string[]
+---@param lhs string
+---@param repeat_key string
+---@param rhs string | function
+---@param opts? vim.keymap.set.Opts
+M.map_submode = function(mode, lhs, repeat_key, rhs, opts)
+  local plug_name = ""
+  if type(mode) == "string" then
+    plug_name = "<Plug>(" .. lhs .. mode .. ")"
+  else
+    plug_name = "<Plug>(" .. lhs .. ")"
+  end
+  vim.keymap.set(mode, lhs, rhs .. plug_name, opts)
+  vim.keymap.set(mode, plug_name .. repeat_key, rhs .. plug_name)
+  -- code
+end
+
+-- Toggle option
+---@param option string
+---@param silent boolean?
+---@param values? {[1]:any, [2]:any}
+M.toggle = function(option, silent, values)
+  if values then
+    if vim.opt_local[option]:get() == values[1] then
+      vim.opt_local[option] = values[2]
+    else
+      vim.opt_local[option] = values[1]
+    end
+    vim.notify("Set " .. option .. " to " .. vim.opt_local[option]:get())
+    return
+  end
+  vim.opt_local[option] = not vim.opt_local[option]:get()
+  if not silent then
+    if vim.opt_local[option]:get() then
+      vim.notify("Enabled " .. option)
+    else
+      vim.notify("Disabled " .. option)
+    end
   end
 end
 
