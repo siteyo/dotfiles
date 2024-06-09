a77238dd0dea8ad6168a75f246be937e6f017f30
Saito Yuki <phicyanoc@gmail.com>
2024-06-09 23:06:38 +0900

    wezterm: remove battery status

--- config/wezterm/events/update-right-status.lua
+++ config/wezterm/events/update-right-status.lua
@@ -42,52 +42,52 @@ local date_gen = function()
   return util.cat_icon(icon, date)
 end
 
--- battery
----@param state string
----@param state_of_charge number
----@return string
-local battery_icon_gen = function(state, state_of_charge)
-  local icon = ""
-  if state_of_charge > 0.98 then
-    icon = wez.nerdfonts.md_battery
-  elseif state_of_charge > 0.9 then
-    icon = wez.nerdfonts.md_battery_90
-  elseif state_of_charge > 0.8 then
-    icon = wez.nerdfonts.md_battery_80
-  elseif state_of_charge > 0.7 then
-    icon = wez.nerdfonts.md_battery_70
-  elseif state_of_charge > 0.6 then
-    icon = wez.nerdfonts.md_battery_60
-  elseif state_of_charge > 0.5 then
-    icon = wez.nerdfonts.md_battery_50
-  elseif state_of_charge > 0.4 then
-    icon = wez.nerdfonts.md_battery_40
-  elseif state_of_charge > 0.3 then
-    icon = wez.nerdfonts.md_battery_30
-  elseif state_of_charge > 0.2 then
-    icon = wez.nerdfonts.md_battery_20
-  elseif state_of_charge > 0.1 then
-    icon = wez.nerdfonts.md_battery_10
-  else
-    icon = wez.nerdfonts.md_battery_alert_variant_outline
-  end
-  if state ~= "Discharging" then
-    icon = icon .. wez.nerdfonts.md_lightning_bolt
-  end
-  return icon
-end
+-- -- battery
+-- ---@param state string
+-- ---@param state_of_charge number
+-- ---@return string
+-- local battery_icon_gen = function(state, state_of_charge)
+--   local icon = ""
+--   if state_of_charge > 0.98 then
+--     icon = wez.nerdfonts.md_battery
+--   elseif state_of_charge > 0.9 then
+--     icon = wez.nerdfonts.md_battery_90
+--   elseif state_of_charge > 0.8 then
+--     icon = wez.nerdfonts.md_battery_80
+--   elseif state_of_charge > 0.7 then
+--     icon = wez.nerdfonts.md_battery_70
+--   elseif state_of_charge > 0.6 then
+--     icon = wez.nerdfonts.md_battery_60
+--   elseif state_of_charge > 0.5 then
+--     icon = wez.nerdfonts.md_battery_50
+--   elseif state_of_charge > 0.4 then
+--     icon = wez.nerdfonts.md_battery_40
+--   elseif state_of_charge > 0.3 then
+--     icon = wez.nerdfonts.md_battery_30
+--   elseif state_of_charge > 0.2 then
+--     icon = wez.nerdfonts.md_battery_20
+--   elseif state_of_charge > 0.1 then
+--     icon = wez.nerdfonts.md_battery_10
+--   else
+--     icon = wez.nerdfonts.md_battery_alert_variant_outline
+--   end
+--   if state ~= "Discharging" then
+--     icon = icon .. wez.nerdfonts.md_lightning_bolt
+--   end
+--   return icon
+-- end
 
----@return string
-local battery_gen = function()
-  local icon = ""
-  local battery = ""
-  for _, b in ipairs(wez.battery_info()) do
-    icon = battery_icon_gen(b.state, b.state_of_charge)
-    battery = string.format("%.0f%%", b.state_of_charge * 100)
-    battery = util.cat_icon(icon, string.format("%.0f%%", b.state_of_charge * 100))
-  end
-  return battery
-end
+-- -- -@return string
+-- local battery_gen = function()
+--   local icon = ""
+--   local battery = ""
+--   for _, b in ipairs(wez.battery_info()) do
+--     icon = battery_icon_gen(b.state, b.state_of_charge)
+--     battery = string.format("%.0f%%", b.state_of_charge * 100)
+--     battery = util.cat_icon(icon, string.format("%.0f%%", b.state_of_charge * 100))
+--   end
+--   return battery
+-- end
 
 wez.on("update-status", function(window)
   local pane = window:active_pane()
@@ -99,7 +99,7 @@ wez.on("update-status", function(window)
 
   local file_path = file_path_gen(pane)
   local date = date_gen()
-  local bat = battery_gen()
+  -- local bat = battery_gen()
 
   local div_icon = wez.nerdfonts.pl_right_hard_divider
 
@@ -119,13 +119,13 @@ wez.on("update-status", function(window)
     { Background = { Color = bg_colors[2] } },
     { Foreground = { Color = fg } },
     { Text = date },
+    --
+    -- { Background = { Color = bg_colors[2] } },
+    -- { Foreground = { Color = bg_colors[3] } },
+    -- { Text = div_icon },
 
-    { Background = { Color = bg_colors[2] } },
-    { Foreground = { Color = bg_colors[3] } },
-    { Text = div_icon },
-
-    { Background = { Color = bg_colors[3] } },
-    { Foreground = { Color = fg } },
-    { Text = bat },
+    -- { Background = { Color = bg_colors[3] } },
+    -- { Foreground = { Color = fg } },
+    -- { Text = bat },
   }))
 end)
