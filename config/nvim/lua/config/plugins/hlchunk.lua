6a587b244e73de47eac29d6c2825c38e525ecbd4
Saito Yuki <phicyanoc@gmail.com>
2024-06-10 21:58:33 +0900

    neovim: remove modified chars settings

--- config/nvim/lua/config/plugins/hlchunk.lua
+++ config/nvim/lua/config/plugins/hlchunk.lua
@@ -9,13 +9,6 @@ local M = {
         { fg = "#c21f30" },
       },
       use_treesitter = true,
-      chars = {
-        horizontal_line = "━",
-        vertical_line = "┃",
-        left_top = "┏",
-        left_bottom = "┗",
-        right_arrow = "━",
-      },
     },
     indent = {
       enable = false,
