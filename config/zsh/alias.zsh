6c8335715de8bbe788ee9375ad23b9b737c982ec
Saito Yuki <phicyanoc@gmail.com>
2024-06-12 23:13:13 +0900

    zsh: organize how to load settings using sheldon

---
+++ config/zsh/alias.zsh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+# Aliases
+# --------------------------------------------------------------------
+## eza
+if [[ $(command -v eza) ]]; then
+  alias ls='eza --icons --git'
+  alias ll='eza --icons --git -l'
+  alias llc='eza --icons --git -l -s changed'
+  alias la='eza --icons --git -a'
+  alias lal='eza --icons --git -al'
+  alias lalc='eza --icons --git -al -s changed'
+  alias lt='eza -T -L 3 -a -I "node_modules|.git|.cache" --icons'
+  alias ltl='eza -T -L 3 -a -I "node_modules|.git|.cache" -l --icons'
+else
+  alias ll='ls -l'
+  alias la='ls -a'
+  alias lal='ls -al'
+fi
+
+## cd
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
+## WSL
+if [[ $(uname -r) == *microsoft* ]]; then
+  alias whome='cd $(wslpath $(wslvar USERPROFILE))'
+fi
+
+## bat
+if command -v bat >/dev/null; then
+  alias bat="bat --theme=\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo Nord || echo GitHub)"
+fi
---
+++ config/zsh/tools.zsh
@@ -0,0 +1,53 @@
+# Tools
+# --------------------------------------------------------------------
+
+## aqua
+if command -v aqua >/dev/null; then
+  source <(aqua completion zsh)
+  export PATH="${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}aquaproj-aqua}/bin:$PATH"
+  export AQUA_GLOBAL_CONFIG="${HOME}/.config/aqua/aqua.yaml"
+  export AQUA_PROGRESS_BAR=true
+  aqua i -l -a
+fi
+
+# fzf
+[ -f "${HOME}/.fzf.zsh" ] &&
+  source "${HOME}/.fzf.zsh"
+
+## zoxide
+if command -v zoxide >/dev/null; then
+  eval "$(zoxide init zsh)"
+fi
+
+## cargo (Rust)
+if [ -d "${HOME}/.cargo" ]; then
+  source "${HOME}/.cargo/env"
+  [ ! -f "${COMPLETIONS_PATH}/_rustup" ] &&
+    $(command -v rustup) completions zsh >"${COMPLETIONS_PATH}/_rustup"
+  [ ! -f "${COMPLETIONS_PATH}/_cargo" ] &&
+    $(command -v rustup) completions zsh cargo >"${COMPLETIONS_PATH}/_cargo"
+fi
+
+## rye (Python)
+if [ -d "${HOME}/.rye" ]; then
+  source "${HOME}/.rye/env"
+  [ ! -f "${COMPLETIONS_PATH}/_rye" ] &&
+    $(command -v rye) self completion -s zsh >"${COMPLETIONS_PATH}/_rye"
+fi
+
+## mise
+if command -v mise >/dev/null; then
+  eval "$(mise activate zsh)"
+fi
+
+## yazi
+if command -v yazi >/dev/null; then
+  function ya() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+      cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
+  }
+fi
