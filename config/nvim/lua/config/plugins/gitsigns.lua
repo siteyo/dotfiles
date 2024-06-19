37b39047593165dad9c54ffe3795bb09bdff9969
Saito Yuki <phicyanoc@gmail.com>
2024-06-20 00:01:45 +0900

    fix: delete deprecated settings #243

--- config/nvim/lua/config/plugins/gitsigns.lua
+++ config/nvim/lua/config/plugins/gitsigns.lua
@@ -8,11 +8,12 @@ function M.config()
   local gitsigns = require("gitsigns")
   gitsigns.setup({
     signs = {
-      add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-      change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-      delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+      add = { text = "┃" },
+      change = { text = "┃" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "┆" },
     },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
     numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -42,9 +43,6 @@ function M.config()
       relative = "cursor",
       row = 0,
       col = 1,
-    },
-    yadm = {
-      enable = false,
     },
     on_attach = function(bufnr)
       local function map(mode, lhs, rhs, opts)
