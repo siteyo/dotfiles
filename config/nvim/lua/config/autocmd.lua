3ce57dea6b63228eeefcc2c51837f28a6f3d7764
Saito Yuki <phicyanoc@gmail.com>
2024-07-03 18:56:36 +0900

    fix: autocmd for neorg

--- config/nvim/lua/config/autocmd.lua
+++ config/nvim/lua/config/autocmd.lua
@@ -1,6 +1,6 @@
 local neorg = vim.api.nvim_create_augroup("neorg", { clear = true })
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "norg",
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+  pattern = "*.norg",
   group = neorg,
   command = "setlocal conceallevel=2",
 })
