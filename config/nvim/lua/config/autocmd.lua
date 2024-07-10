f9b7a89beeb035b180dfde3f8ed7e8ac4faba8b8
Saito Yuki <phicyanoc@gmail.com>
2024-07-10 21:42:30 +0900

    neovim: fix event

--- config/nvim/lua/config/autocmd.lua
+++ config/nvim/lua/config/autocmd.lua
@@ -1,5 +1,5 @@
 local neorg = vim.api.nvim_create_augroup("neorg", { clear = true })
-vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+vim.api.nvim_create_autocmd({ "BufReadPre" }, {
   pattern = "*.norg",
   group = neorg,
   command = "setlocal conceallevel=2",
