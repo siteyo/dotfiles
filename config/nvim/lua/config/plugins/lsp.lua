cef7d3943055d30bf14d868fa93b6683d614cfe5
Saito Yuki <phicyanoc@gmail.com>
2024-12-24 01:18:31 +0900

    neovim: add lazydev.nvim

--- config/nvim/lua/config/plugins/lsp.lua
+++ config/nvim/lua/config/plugins/lsp.lua
@@ -37,7 +37,6 @@ return {
       -- "williamboman/mason-lspconfig.nvim",
       "hrsh7th/cmp-nvim-lsp",
       -- "jose-elias-alvarez/typescript.nvim",
-      { "folke/neodev.nvim", config = true },
     },
     event = "BufReadPre",
     opts = {
