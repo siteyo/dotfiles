ca7f4c807b288f1244f76699ad55e24dd1b3a3b7
Saito Yuki <phicyanoc@gmail.com>
2025-01-13 01:31:22 +0900

    sketchybar: modified workspaces area

--- config/sketchybar/plugins/aerospace.sh
+++ config/sketchybar/plugins/aerospace.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
-# shellcheck disable=SC1091
+# shellcheck disable=SC1091,SC1083
 source "$CONFIG_DIR/color.sh"
 source "$CONFIG_DIR/icons.sh"
 
+# Dual monitor support only
+FOCUSED_MONITOR=$(aerospace list-monitors --focused --format %{monitor-id})
+UNFOCUSED_MONITOR=$(aerospace list-monitors --focused no --format %{monitor-id})
+
 VISIBLE=$(aerospace list-workspaces --visible --monitor all)
-NOEMPTY=$(aerospace list-workspaces --empty no --monitor all)
+NOEMPTY=$(aerospace list-workspaces --empty no --monitor all --format m%{monitor-id}w%{workspace})
 if printf '%s\n' "${VISIBLE[@]}" | grep -qx "$1"; then
   if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --set "$NAME" background.drawing=on background.color="$COLOR_CYAN"
   else
     sketchybar --set "$NAME" background.drawing=on background.color="$COLOR_RED"
   fi
-elif printf '%s\n' "${NOEMPTY[@]}" | grep -qx "$1"; then
+elif printf '%s\n' "${NOEMPTY[@]}" | grep -qx "m${FOCUSED_MONITOR}w$1"; then
   sketchybar --set "$NAME" background.drawing=on background.color="$COLOR_FG"
+elif printf '%s\n' "${NOEMPTY[@]}" | grep -qx "m${UNFOCUSED_MONITOR}w$1"; then
+  sketchybar --set "$NAME" background.drawing=on background.color="$COLOR_RED"
 else
   sketchybar --set "$NAME" background.drawing=off
 fi
