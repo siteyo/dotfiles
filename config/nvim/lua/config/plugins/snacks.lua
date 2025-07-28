10d93f4c53f836dbe824778d33060f7519b4e75d
Saito Yuki <phicyanoc@gmail.com>
2025-07-28 21:30:36 +0900

    neovim: preview wiki-style images with obsidian.nvim

--- config/nvim/lua/config/plugins/snacks.lua
+++ config/nvim/lua/config/plugins/snacks.lua
@@ -89,7 +89,13 @@ local M = {
         end,
       },
     },
-    image = {},
+    image = {
+      resolve = function(path, src)
+        if require("obsidian.api").path_is_note(path) then
+          return require("obsidian.api").resolve_image_path(src)
+        end
+      end,
+    },
     input = {},
     scroll = {},
     statuscolumn = {},
