a84fca8a2053400ad256291e3e71a769e630306b
Saito Yuki <phicyanoc@gmail.com>
2025-08-17 23:07:37 +0900

    neovim: modified lspconfig setting

--- config/nvim/lua/config/plugins/lsp.lua
+++ config/nvim/lua/config/plugins/lsp.lua
@@ -85,7 +85,12 @@ return {
     },
     config = function(_, opts)
       local servers = opts.servers
-      local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        require("cmp_nvim_lsp").default_capabilities()
+      )
 
       local function setup(server)
         local server_opts = servers[server] == true and {} or servers[server]
