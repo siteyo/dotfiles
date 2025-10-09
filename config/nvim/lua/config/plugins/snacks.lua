49201421e134cfee811bcf12b2ca495833b54852
Saito Yuki <phicyanoc@gmail.com>
2025-10-09 22:55:03 +0900

    neovim: modified snacks.nvim

--- config/nvim/lua/config/plugins/snacks.lua
+++ config/nvim/lua/config/plugins/snacks.lua
@@ -8,15 +8,15 @@ local M = {
         pick = "telescope.nvim",
         keys = {
           -- stylua: ignore start
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "e", desc = "Explorer", action = ":Oil" },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":Telescope smart_open" },
-          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "e", desc = "Explorer", action = ":Oil" },
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "g", desc = "Git Files", action = ":Telescope git_files" },
+          { icon = " ", key = "p", desc = "GitHub PR List", action = ":Octo pr list" },
           { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-          { icon = "󰚰 ", key = "u", desc = "Lazy Update", action = ":Lazy update", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
           -- stylua: ignore end
         },
@@ -24,8 +24,6 @@ local M = {
       sections = {
         { section = "header" },
         { section = "keys", gap = 1, padding = 2 },
-        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 2 },
-        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
         { section = "startup" },
         {
           pane = 2,
@@ -57,7 +55,7 @@ local M = {
               icon = " ",
               title = "Open PRs",
               cmd = "gh pr list -L 3",
-              key = "p",
+              key = "P",
               action = function()
                 vim.fn.jobstart("gh pr list --web", { detach = true })
               end,
