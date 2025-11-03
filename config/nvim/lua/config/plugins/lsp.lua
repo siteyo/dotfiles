abf1e4cf42efb4488d6f633ef38ee6c54adcc92c
Saito Yuki <phicyanoc@gmail.com>
2025-11-03 23:12:48 +0900

    neovim: update lsp setting

--- config/nvim/lua/config/plugins/lsp.lua
+++ config/nvim/lua/config/plugins/lsp.lua
@@ -1,49 +1,13 @@
-local enabled = true
-
-return {
-  -- mason
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    opts = {
-      ensure_installed = {
-        -- "editorconfig-checker",
-        -- "markdownlint",
-        -- "prettierd",
-        -- "selene",
-        "shellcheck",
-        "shfmt",
-        "stylua",
-      },
-    },
-    config = function(_, opts)
-      require("mason").setup(opts)
-      local mr = require("mason-registry")
-      for _, tool in ipairs(opts.ensure_installed) do
-        local p = mr.get_package(tool)
-        if not p:is_installed() then
-          p:install()
-        end
-      end
-    end,
-    enabled = false,
-  },
-
-  -- lspconfig
+local M = {
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      -- "williamboman/mason.nvim",
-      -- "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-      -- "jose-elias-alvarez/typescript.nvim",
-    },
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
     event = "BufReadPre",
     opts = {
       diagnostics = {
         underline = true,
         update_in_insert = false,
-        virtual_text = { spacing = 4, prefix = "●" },
+        virtual_text = { spacing = 4, source = "if_many", prefix = "●" },
         severity_sort = true,
       },
       servers = {
@@ -52,12 +16,9 @@ return {
         dockerls = true,
         emmet_ls = true,
         jsonls = true,
-        -- marksman = true,
-        -- pylsp = true,
         ruff = true,
         pyright = true,
         rust_analyzer = true,
-        -- sqlls = true,
         sqls = true,
         ts_ls = true,
         lua_ls = {
@@ -85,6 +46,9 @@ return {
       },
     },
     config = function(_, opts)
+      -- diagnostics
+      vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+
       local servers = opts.servers
       local capabilities = vim.tbl_deep_extend(
         "force",
@@ -105,33 +69,16 @@ return {
             return
           end
         end
-        require("lspconfig")[server].setup(server_opts)
+        vim.lsp.config(server, server_opts)
+        vim.lsp.enable(server)
+        -- require("lspconfig")[server].setup(server_opts)
       end
 
-      -- local mlsp = require("mason-lspconfig")
-      -- local available = mlsp.get_available_servers()
-
-      -- local ensure_installed = {}
-      for server, server_opts in pairs(servers) do
-        if server_opts then
-          server_opts = server_opts == true and {} or server_opts
-          -- if server_opts.mason == false or not vim.tbl_contains(available, server) then
-          setup(server)
-          -- else
-          --   ensure_installed[#ensure_installed + 1] = server
-          -- end
-        end
+      for server, _ in pairs(servers) do
+        setup(server)
       end
-
-      -- mlsp.setup({ ensure_installed = ensure_installed })
-      -- mlsp.setup_handlers({ setup })
     end,
-    enabled = enabled,
   },
-
-  -- {
-  --   "j-hui/fidget.nvim",
-  --   event = "BufReadPre",
-  --   config = true,
-  -- },
 }
+
+return M
