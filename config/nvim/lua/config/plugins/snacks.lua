1e637d14e23bb21fce79af23264b4f14d6f1ec97
Saito Yuki <phicyanoc@gmail.com>
2025-12-31 16:19:40 +0900

    feat(neovim): disabled obsidian.nvim

--- config/nvim/lua/config/plugins/snacks.lua
+++ config/nvim/lua/config/plugins/snacks.lua
@@ -87,13 +87,7 @@ local M = {
         end,
       },
     },
-    image = {
-      resolve = function(path, src)
-        if require("obsidian.api").path_is_note(path) then
-          return require("obsidian.api").resolve_image_path(src)
-        end
-      end,
-    },
+    image = {},
     input = {},
     scroll = {},
     statuscolumn = {},
