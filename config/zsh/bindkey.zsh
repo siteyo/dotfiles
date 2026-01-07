9997a362ec15c8e199f5da610ffe2e35e3b14012
Saito Yuki <phicyanoc@gmail.com>
2026-01-08 00:03:27 +0900

    feat: Add ghq integration and new tools
    - Adds a zsh function and keybinding to quickly jump to a git repository managed by ghq.
    - Updates the Brewfile with new applications and CLI tools, including uv, gemini-cli, and obsidian.
    - Replaces 'rye' with 'uv' in the README.

--- config/zsh/bindkey.zsh
+++ config/zsh/bindkey.zsh
@@ -3,3 +3,5 @@
 autoload -Uz edit-command-line
 zle -N edit-command-line
 bindkey "^O" edit-command-line
+bindkey "^G" _ghq_src
+bindkey "^XG" list-expand
--- config/zsh/function.zsh
+++ config/zsh/function.zsh
@@ -18,3 +18,13 @@
 # }
 # zle -N _task_add_nvim
 # bindkey '^O' _task_add_nvim
+
+_ghq_src() {
+  local selected_dir=$(ghq list | fzf --prompt="repo > " --layout="reverse" --query="$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd $(ghq root)/${selected_dir}"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N _ghq_src
